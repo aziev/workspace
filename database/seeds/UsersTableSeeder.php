5c3b42af0f33cf7e0e7c2d5d2fd2405d48abe8b2
mik <mikail.aziev@gmail.com>
2018-08-29 17:00:23 +0300

    create authentication

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,48 +17,56 @@ class UsersTableSeeder extends Seeder
         $password = bcrypt('123456');
 
         User::insert([[
+            'email' => 'ibrain090@gmail.com',
             'name' => 'А. Ибрагим',
             'avatar' => 'img/ibragim_aziev.jpg',
             'position' => 1,
             'payed' => true,
             'password' => $password,
         ], [
+            'email' => null,
             'name' => 'Ч. Ахмед',
             'avatar' => 'img/ahmed_chergizov.jpg',
             'position' => 2,
             'payed' => false,
             'password' => $password,
         ], [
+            'email' => null,
             'name' => 'А. Хаваж',
             'avatar' => 'img/khavazh_archakov.jpg',
             'position' => 3,
             'payed' => false,
             'password' => $password,
         ], [
+            'email' => null,
             'name' => 'Т. Ахмед',
             'avatar' => 'img/ahmed_timurziev.jpg',
             'position' => 4,
             'payed' => false,
             'password' => $password,
         ], [
+            'email' => null,
             'name' => 'Т. Яхья',
             'avatar' => '',
             'position' => 5,
             'payed' => false,
             'password' => $password,
         ], [
+            'email' => null,
             'name' => 'А. Берд',
             'avatar' => 'img/berd_aziev.jpg',
             'position' => 10,
             'payed' => true,
             'password' => $password,
         ], [
+            'email' => null,
             'name' => 'А. Микаил',
             'avatar' => 'img/mikail_aziev.jpg',
             'position' => 11,
             'payed' => false,
             'password' => $password,
         ], [
+            'email' => null,
             'name' => 'Д. Абдулла',
             'avatar' => '',
             'position' => 12,
